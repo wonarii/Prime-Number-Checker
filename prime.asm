37a175b76638cbe802d9d79d39b05db61bcacc04
wonarii <ariellecwong@gmail.com>
2025-04-03 09:41:44 -0400

    part 1 code
    tried my best
    i don't think it works:(

--- prime.asm
+++ prime.asm
@@ -13,6 +13,13 @@ section .data
 	prime_msg db  'Number is prime', 0x0a
         not_prime_msg db 'Number is NOT prime', 0x0a
 
+	;variables for gettting inputs
+	ask_number_msg db 'Please enter a number between 0 and 255', 0x0a ; asking user for a number
+	minNumber db '0', 0x0a			; 
+	maxNumber db '255', 0x0a		;
+	error_msg db 'Incorrect input, try again', 0x0a 		; if the input is invalid
+
+	
 
 ; ### PUT CODE (SUBROUTINES) IN HERE ### 
 section .text 
@@ -24,14 +31,48 @@ section .text
 ; ### This function will ask the user to input a number and store it in "number"
 ; ### If the input is invalid, then either ask again or use default number (UP TO YOU)
 get_inputs: 
+				;ASK FOR NUMBER
+	mov eax, 4		;
+	mov ebx, 1		;
+	mov ecx,ask_number_msg	;
+	mov edx, 39		;length of message
+	int 80h		;displays the message
 
+				;COLLECT NUMBER
+	mov eax, 3		;
+	mov ebx, 0		;
+	mov ecx, number		;will put collected input into number
+	mov edx, 3		;the length of the input should not exceed 3 characters (0-255)
+	int 80h			;collect the message
 
+	
 
+				;CHECK min
+	mov eax, number		; puts number in eax
+	mov ebx, minNumber	;puts minNumber in ebx
+	cmp eax, ebx		;comparing number with 0
+	jl ASKAGAIN 		; number < 0, jumps back to asking
 
+				;CHECK max
+	mov eax, number		; puts number in eax
+	mov ebx, maxNumber	; puts max number in ebx
+	cmp eax, ebx		;comparing number with 255 
+	jg ASKAGAIN		; number > 255, jumps to ask again
 
+	;IF THE NUMBER IS VALID
+				;TRANSFORM STRING TO INT
+	sub ecx, '0'		;to un-ascii
+	mov [number], ecx	;puts un-ascii number in number
 
-
-
+	jmp divide_by_two	;GOING TO NEXT STEP 
+	
+ASKAGAIN:
+	mov eax, 4		;
+	mov ebx, 1		;
+	mov ecx,error_msg	;
+	mov edx, 26		;length of message
+	int 0x80		;displays the message
+	jmp get_inputs		;goes back to get_inputs to ask again
 
 
 ; ### DIVIDE BY 2 ###
@@ -85,7 +126,7 @@ display_not_prime:
 ; ### MAIN FUNCTION ###
 ; ### We will use this to call the subroutines and simple code
 _start: 
-
+	jmp get_inputs		;jumps to get inputs
 
 
 
